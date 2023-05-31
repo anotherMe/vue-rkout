fd2d2da1edbcbd6d2b0a2ab6a60e6ba5e0aa5e79
m.giordano <m.giordano@sis-net.it>
2023-05-31 17:56:22 +0200

    First commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+
+    host: 'localhost',
+    port: 8089,
+    title: 'Hello',
+    description: 'Just playing around'
+
+  }
---
+++ docs/README.md
@@ -0,0 +1,8 @@
+
+# Hello
+
+## there
+
+Black Pearl
+
+![Just a test](/images/test-200x300.jpg)
